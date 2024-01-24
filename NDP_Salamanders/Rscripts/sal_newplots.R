277ad73413fb04555ed0e36f8b493c4c193e36c6
Alfredo Ascanio <ascanio.alfredoa@gmail.com>
2024-01-24 16:35:49 -0500

    Update figures on Salamander NDP

--- NDP_Salamanders/Rscripts/sal_newplots.R
+++ NDP_Salamanders/Rscripts/sal_newplots.R
@@ -5,9 +5,9 @@ library(tidyterra)
 library(RColorBrewer)
 library(rnaturalearth) # boundary data
 library(ggrepel)
-#library(gridExtra)
-#library(ggpubr)
-#library(cowplot)
+library(gridExtra)
+library(ggpubr)
+library(cowplot)
 
 #### Read NDP data ####
 rc <- read_rds("Results/NDP_Salamanders_ResponseCurves2.rds")
@@ -62,15 +62,19 @@ p1
 
 #### Plot NDP ###
 p2 <- ndp %>%
-    ggplot(aes(x = exclusivity, y = dissimilarity, label = var, fill = vartype)) +
+    ggplot(aes(x = exclusivity, y = dissimilarity, label = var, fill = vartype, 
+               shape = vartype)) +
     xlim(0, 1) + ylim(0, 1) +
     xlab("Niche Exclusivity") + ylab("Niche Dissimilarity") +
     geom_hline(yintercept = 0.5) +
     geom_vline(xintercept = 0.5) +
-    geom_point(size = 4, shape = 21, alpha = 0.8) +
-    geom_text_repel(size = 4) +
+    geom_point(size = 6, alpha = 0.8) +
+    geom_text_repel(size = 5) +
     scale_fill_manual("Variable type", 
                       values = c("#62A39F", "#000000", "#2F8745")) +
+    theme_bw() +
+    scale_shape_manual("Variable type", 
+                      values = c(21, 22, 23)) +
     theme_bw() +
     theme(#legend.position = "bottom",
         legend.position = c(0.87, 0.75),
@@ -80,41 +84,45 @@ p2 <- ndp %>%
 p2
 
 #### Plot response curves ####
-rc %>% 
-    #filter(var %in% c("")) %>%
-    #mutate(var = case_when(
-        #var == "bio1" ~ "Annual Mean Temperature (°C)",
-        #var == "bio2" ~ "Mean Diurnal Range (°C)",
-        #var == "bio3" ~ "Isothermality (°C)",
-        #var == "bio4" ~ "Temperature Seasonality (°C/100)",
-        #var == "bio5" ~ "Max Temperature of Warmest Month (°C)",
-        #var == "bio6" ~ "Min Temperature of Coldest Month (°C) [bio06]",
-        #var == "bio7" ~ "Temperature Annual Range(°C)",
-        #var == "bio8" ~ "Mean Temperature of Wettest Quarter (°C)",
-        #var == "bio9" ~ "Mean Temperature of Warmest Quarter (°C)",
-        #var == "bio10" ~ "Mean Temperature of Coldest Quarter (°C)",
-        #var == "bio11" ~ "Mean Temperature of Coldest Quarter (°C)",
-        #var == "bio12" ~ "Annual Precipitation (kg/m^2) [bio12]",
-        #var == "bio13" ~ "Precipitation of Wettest Month (kg/m^2)",
-        #var == "bio14" ~ "Precipitation of Driest Month (kg/m^2)",
-        #var == "bio15" ~ "Precipitation Seasonality (kg/m^2)",
-        #var == "bio16" ~ "Precipitation of Wettest Quarter (kg/m^2)",
+p3 <- 
+    rc %>% 
+    filter(var %in% c("bio1", "bio3", "bio12", "bio14", "Sand", "WC3rdbar")) %>%
+    mutate(var = case_when(
+        var == "bio1" ~ "Annual Mean Temperature (°C) [bio1]",
+        #var == "bio2" ~ "Mean Diurnal Range (°C) [bio2]",
+        var == "bio3" ~ "Isothermality (°C) [bio3]",
+        #var == "bio4" ~ "Temperature Seasonality (°C/100) [bio4]",
+        #var == "bio5" ~ "Max Temperature of Warmest Month (°C) [bio5]",
+        #var == "bio6" ~ "Min Temperature of Coldest Month (°C) [bio6]",
+        #var == "bio7" ~ "Temperature Annual Range(°C) [bio7]",
+        #var == "bio8" ~ "Mean Temperature of Wettest Quarter (°C) [bio8]",
+        #var == "bio9" ~ "Mean Temperature of Warmest Quarter (°C) [bio9]",
+        #var == "bio10" ~ "Mean Temperature of Coldest Quarter (°C) [bio10]",
+        #var == "bio11" ~ "Mean Temperature of Coldest Quarter (°C) [bio11]",
+        var == "bio12" ~ "Annual Precipitation (kg/m^2) [bio12] [bio12]",
+        #var == "bio13" ~ "Precipitation of Wettest Month (kg/m^2) [bio13]",
+        var == "bio14" ~ "Precipitation of Driest Month (kg/m^2) [bio14]",
+        #var == "bio15" ~ "Precipitation Seasonality (kg/m^2) [bio15]",
+        #var == "bio16" ~ "Precipitation of Wettest Quarter (kg/m^2) [bio16]",
         #var == "bio17" ~ "Precipitation of Driest Quarter (kg/m^2) [bio17]",
-        #var == "bio18" ~ "Precipitation of Warmest Quarter (kg/m^2)",
-        #var == "bio19" ~ "Precipitation of Coldest Quarter (kg/m^2)",
-        #var == "aws.x" ~ "Available Water Storage (mL)",
-        #var == "aws.y" ~ "Available Water Supply (mL)",
-        #var == "awc_r" ~ "Available Water Capacity (mL) [awc_r]",
-        #var == "ffd_r" ~ "Frost Free Days (Julian Days)",                                                                            
-        #var =="resdept_r" ~ "Depth to Any Soil Restricted Layer (cm)",
-        #var == "sandtotal_r" ~ "Sand Total (%)",
-        #var == "claytotal_r" ~ "Clay Total (%) [claytotal_r]",
-        #var == "silttotal_r" ~ "Silt Total (%)",
-        #var == "om_r" ~ "Organic Matter (%)",
-        #var == "soimoistdept_r" ~ "Depth to water table (cm) [soimoistdept_r]",
-        #var == "dbovendry_r" ~ "Db Oven Dry (g/mL)",
-        #var == "dbthirdbar_r" ~ "Db 0.33 Bar H2O (g/mL)"
-    #)) %>%
+        #var == "bio18" ~ "Precipitation of Warmest Quarter (kg/m^2) [bio18]",
+        #var == "bio19" ~ "Precipitation of Coldest Quarter (kg/m^2) [bio19]",
+        #var == "AWC" ~ "Available Water Capacity (mL) [AWC]",
+        #var == "AWS" ~ "Available Water Storage (mL) [AWS]",
+        #var == "CaC03" ~ "Calcium Carbonate Equivalent [CaCO3]",                                                                            
+        #var == "Clay" ~ "Clay Total (%)",
+        #var == "Db3rdbar" ~ "Bulk Density at 1/3 bar (g/mL) [Db3rdbar]",
+        #var == "Dep2AnyRes" ~ "Depth to Any Soil Restricted Layer (cm) [Dep2AnyRes]",
+        #var == "Dep2WatTbl" ~ "Depth to water table (cm) [Dep2WatTbl]",
+        #var == "FrostFDays" ~ "Frost Free Days (Julian Days) [FrostFDays]",                                                                            
+        #var == "LiqLim" ~ "Liquid Limit [LiqLim]",
+        #var == "OrgMatter" ~ "Organic Matter (%)",
+        #var == "pHwater" ~ "pH",
+        var == "Sand" ~ "Sand Total (%)",
+        #var == "Silt" ~ "Silt Total (%)",
+        #var == "WC15Bar" ~ "Water Content at 15 Bar (%) [WC15Bar]",
+        var == "WC3rdbar" ~ "Water Content at 1/3 Bar (%) [WC3rdbar]"
+    )) %>%
     ggplot(aes(x = layer, y = value, color = source)) +
     geom_line(linewidth = 2, alpha = 0.7) +
     facet_wrap(vartype ~ var, scales = "free_x") +
@@ -124,3 +132,88 @@ rc %>%
     theme_bw() +
     theme(legend.position = "top")
 p3
+
+gt <- arrangeGrob(p1 + theme(legend.text = element_text(size = 14)) +
+                      guides(color = guide_legend(override.aes = list(size=3))), 
+                  p2 + theme(axis.title = element_text(size = 20),
+                             legend.text = element_text(size = 20),
+                             text = element_text(size = 20)), 
+                  p3 + theme(text = element_text(size = 16),
+                             legend.position = "none",
+                             axis.title = element_text(size = 20)),
+                  ncol = 2, nrow = 2, 
+                  layout_matrix = rbind(c(1, 1, 3, 3, 3, 3, 3),
+                                        c(1, 1, 3, 3, 3, 3, 3),
+                                        c(1, 1, 3, 3, 3, 3, 3),
+                                        c(NA, 2, 2, 2, 2, 2, NA),
+                                        c(NA, 2, 2, 2, 2, 2, NA),
+                                        c(NA, 2, 2, 2, 2, 2, NA),
+                                        c(NA, 2, 2, 2, 2, 2, NA)))
+
+# Add labels to the arranged plots
+p <- as_ggplot(gt) +                                # transform to a ggplot
+    draw_plot_label(label = c("A)", "B)", "C)"), size = 20,
+                    x = c(-0.005, 0.30, 0.14), y = c(1, 1, 0.58)) # Add labels
+#x = c(-0.005, 0.49, 0.49), y = c(1, 1, 0.5)) # Add labels
+#x = c(-0.005, -0.005, -0.005), y = c(1, 2, 3)) # Add labels
+p
+
+pdf("Figures/Amacopac_NDP.pdf", width = 19, height = 15)
+p
+dev.off()
+
+png("Figures/Amacopac_NDP.png", width = 11000, height = 9000, res = 600)
+p
+dev.off()
+
+
+########### Supplementary figure with all response curves ###############
+p4 <- 
+    rc %>% 
+    #filter(var %in% c("")) %>%
+    mutate(var = case_when(
+        var == "bio1" ~ "Annual Mean Temperature (°C) [bio1]",
+        var == "bio2" ~ "Mean Diurnal Range (°C) [bio2]",
+        var == "bio3" ~ "Isothermality (°C) [bio3]",
+        var == "bio4" ~ "Temperature Seasonality (°C/100) [bio4]",
+        var == "bio5" ~ "Max Temperature of Warmest Month (°C) [bio5]",
+        var == "bio6" ~ "Min Temperature of Coldest Month (°C) [bio6]",
+        var == "bio7" ~ "Temperature Annual Range(°C) [bio7]",
+        var == "bio8" ~ "Mean Temperature of Wettest Quarter (°C) [bio8]",
+        var == "bio9" ~ "Mean Temperature of Warmest Quarter (°C) [bio9]",
+        var == "bio10" ~ "Mean Temperature of Coldest Quarter (°C) [bio10]",
+        var == "bio11" ~ "Mean Temperature of Coldest Quarter (°C) [bio11]",
+        var == "bio12" ~ "Annual Precipitation (kg/m^2) [bio12] [bio12]",
+        var == "bio13" ~ "Precipitation of Wettest Month (kg/m^2) [bio13]",
+        var == "bio14" ~ "Precipitation of Driest Month (kg/m^2) [bio14]",
+        var == "bio15" ~ "Precipitation Seasonality (kg/m^2) [bio15]",
+        var == "bio16" ~ "Precipitation of Wettest Quarter (kg/m^2) [bio16]",
+        var == "bio17" ~ "Precipitation of Driest Quarter (kg/m^2) [bio17]",
+        var == "bio18" ~ "Precipitation of Warmest Quarter (kg/m^2) [bio18]",
+        var == "bio19" ~ "Precipitation of Coldest Quarter (kg/m^2) [bio19]",
+        var == "AWC" ~ "Available Water Capacity (mL) [AWC]",
+        var == "AWS" ~ "Available Water Storage (mL) [AWS]",
+        var == "CaC03" ~ "Calcium Carbonate Equivalent [CaCO3]",                                                                            
+        var == "Clay" ~ "Clay Total (%)",
+        var == "Db3rdbar" ~ "Bulk Density at 1/3 bar (g/mL) [Db3rdbar]",
+        var == "Dep2AnyRes" ~ "Depth to Any Soil Restricted Layer (cm) [Dep2AnyRes]",
+        var == "Dep2WatTbl" ~ "Depth to water table (cm) [Dep2WatTbl]",
+        var == "FrostFDays" ~ "Frost Free Days (Julian Days) [FrostFDays]",                                                                            
+        var == "LiqLim" ~ "Liquid Limit [LiqLim]",
+        var == "OrgMatter" ~ "Organic Matter (%)",
+        var == "pHwater" ~ "pH",
+        var == "Sand" ~ "Sand Total (%)",
+        var == "Silt" ~ "Silt Total (%)",
+        var == "WC15Bar" ~ "Water Content at 15 Bar (%) [WC15Bar]",
+        var == "WC3rdbar" ~ "Water Content at 1/3 Bar (%) [WC3rdbar]"
+    )) %>%
+    ggplot(aes(x = layer, y = value, color = source)) +
+    geom_line(linewidth = 2, alpha = 0.7) +
+    facet_wrap(vartype ~ var, scales = "free_x") +
+    ylab("Frequency") +
+    xlab("Environmental gradient") +
+    scale_color_manual("Species", values = colpal) +
+    theme_bw() +
+    theme(legend.position = "top")
+p4
+
