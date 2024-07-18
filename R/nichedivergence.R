fb68d241251223e330479ca8eb51f0f0c5a65f16
Alfredo Ascanio <ascanio.alfredoa@gmail.com>
2024-07-18 16:14:56 -0400

    Added Niche Divergence calculation functions

--- R/nichedivergence.R
+++ R/nichedivergence.R
@@ -1,11 +1,58 @@
-C <-
-    A[A$x %in% B$x, ] %>%
-    left_join(B[B$x %in% A$x, ], by = "x", suffix = c("_a", "_b")) %>%
-    pivot_longer(cols = 2:3, names_to = "source_curve", values_to = "y") %>%
-    group_by(x) %>%
-    summarise(y = min(y))
+#' Determines response overlap between two beta function curves from betaPDF()
+#'
+#' @param spa Data frame containing x and y values for Species A response
+#' @param spb Data frame containing x and y values for Species B response
+#'
+#' @return Data frame containing x and y values for the overlap curve between species A and B
+#' @export
+#'
+#' @examples
+#' spa <- betaPDF(a = 0, b = 1, alpha = 3, gamma = 1)
+#' spb <- betaPDF(a = 0, b = 1, alpha = 1, gamma = 3)
+#' beta_overlap(spa, spb)
+beta_overlap <- function(spa, spb) {
+    ov <-
+        spa[spa[, 1] %in% spb[, 1], ] |>
+        dplyr::left_join(spb[spb[, 1] %in% spb[, 1], ], by = "x", suffix = c("_a", "_b")) |>
+        tidyr::pivot_longer(cols = 2:3, names_to = "source_curve", values_to = "y") |>
+        dplyr::group_by(.data$x) |>
+        dplyr::summarise(y = min(.data$y))
+    ov
+}
 
-NicheDiss <- 1 - (trapz(C$x, C$y)/trapz(A$x, A$y) + trapz(C$x, C$y)/trapz(B$x, B$y))/2
+#' Calculate Niche Dissimilarity Index for the Niche Divergence Plane
+#'
+#' @param spa Data frame containing x and y values for Species A response
+#' @param spb Data frame containing x and y values for Species B response
+#' @param ov Data frame containing x and y values for Overlap between A and B
+#'
+#' @return Numeric Niche Dissimilarity value
+#' @export
+#'
+#' @examples
+#' spa <- betaPDF(a = 0, b = 1, alpha = 3, gamma = 1)
+#' spb <- betaPDF(a = 0, b = 1, alpha = 1, gamma = 3)
+#' ov <- beta_overlap(spa, spb)
+#' niche_diss(spa, spb, ov)
+niche_diss <- function(spa, spb, ov) {
+    1 - (pracma::trapz(ov[[1]], ov[[2]])/pracma::trapz(spa[[1]], spa[[2]]) + pracma::trapz(ov[[1]], ov[[2]])/pracma::trapz(spb[[1]], spb[[2]]))/2
+}
 
-RNO <- 1 - (min(c(max(A$x), max(B$x))) - max(c(min(A$x), min(B$x))))/(max(c(max(A$x), max(B$x))) - min(c(min(A$x), min(B$x))))
-if(RNO > 1) RNO <- 1
+#' Calculate Niche Exclusivity Index for the Niche Divergence Plane
+#'
+#' @param spa Data frame containing x and y values for Species A response
+#' @param spb Data frame containing x and y values for Species B response
+#'
+#' @return Numeric Niche Exclusivity value
+#' @export
+#'
+#' @examples
+#' spa <- betaPDF(a = 0, b = 1, alpha = 3, gamma = 1)
+#' spb <- betaPDF(a = 0, b = 1, alpha = 1, gamma = 3)
+#' niche_excl(spa, spb)
+niche_excl <- function(spa, spb) {
+    excl <- 1 - (min(c(max(spa[, 1]), max(spb[, 1]))) - max(c(min(spa[, 1]), min(spb[, 1]))))/(max(c(max(spa[, 1]), max(spb[, 1]))) - min(c(min(spa[, 1]), min(spb[, 1]))))
+    if(excl > 1) excl <- 1
+    excl
+}
+
