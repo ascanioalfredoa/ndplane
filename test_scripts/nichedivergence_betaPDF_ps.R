ef1161f07707e1409974d76394a4aab495d9f102
Alfredo Ascanio <ascanio.alfredoa@gmail.com>
2024-07-19 19:05:14 -0400

    Added progress bar to niche divergence betaPDF loops

--- test_scripts/nichedivergence_betaPDF_ps.R
+++ test_scripts/nichedivergence_betaPDF_ps.R
@@ -22,22 +22,28 @@ results <- dplyr::as_tibble(array(data = 0,
 
 
 tic()
-row_combs |> purrr::map_df(\(x) {
-    i <- x[1]; j <- x[2]
-    spa <- betaPDF_ps(par_space[i, ])
-    spb <- betaPDF_ps(par_space[j, ])
-    ov <- beta_overlap(spa, spb)
-    results[1, ] <- c(i, j,
-                      niche_diss(spa, spb, ov),
-                      niche_excl(spa, spb),
-                      par_space[i, ], par_space[j, ])
-    results
-} )
+progressr::with_progress({
+    p <- progressr::progressor(steps = length(row_combs))
+    row_combs |> purrr::map_df(\(x) {
+        p()
+        i <- x[1]; j <- x[2]
+        spa <- betaPDF_ps(par_space[i, ])
+        spb <- betaPDF_ps(par_space[j, ])
+        ov <- beta_overlap(spa, spb)
+        results[1, ] <- c(i, j,
+                          niche_diss(spa, spb, ov),
+                          niche_excl(spa, spb),
+                          par_space[i, ], par_space[j, ])
+        results
+    })
+}
+)
 toc()
 
 tic()
-future::plan(future::multisession, workers = 2)
-row_combs |> furrr::future_map(\(x) {
+p <- progressor(steps = nrow(row_combs))
+row_combs |> purrr::map_df(\(x) {
+    p()
     i <- x[1]; j <- x[2]
     spa <- betaPDF_ps(par_space[i, ])
     spb <- betaPDF_ps(par_space[j, ])
@@ -47,7 +53,27 @@ row_combs |> furrr::future_map(\(x) {
                       niche_excl(spa, spb),
                       par_space[i, ], par_space[j, ])
     results
-}) |>
+})
+toc()
+
+
+tic()
+progressr::with_progress({
+    p <- progressr::progressor(steps = length(row_combs))
+future::plan(future::multisession, workers = 10)
+row_combs |> furrr::future_map(\(x) {
+    p()
+    i <- x[1]; j <- x[2]
+    spa <- betaPDF_ps(par_space[i, ])
+    spb <- betaPDF_ps(par_space[j, ])
+    ov <- beta_overlap(spa, spb)
+    results[1, ] <- c(i, j,
+                      niche_diss(spa, spb, ov),
+                      niche_excl(spa, spb),
+                      par_space[i, ], par_space[j, ])
+    results
+}, .progress = TRUE) |>
     dplyr::bind_rows()
 future::plan(future::sequential)
+})
 toc()
